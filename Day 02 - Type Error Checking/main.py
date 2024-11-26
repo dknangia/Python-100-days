a4e6b277c602d8edfa5633e3045bf7b10c4f0520
Deepak Nangia <dknangia@gmail.com>
2024-11-25 21:56:16 -0500

    define variable in Python

---
+++ Day 02 - Type Error Checking/main.py
@@ -0,0 +1,22 @@
+
+# Type checking
+print(type("123"))
+print(type(123))
+print(type(12.34))
+print(type(True))
+
+
+# Type casting
+int("234"+"231")
+convert = int("123") + int ("345")
+print(convert)
+
+
+# Rounding the numbers
+print("Rounding of the numbers")
+print(round(30.434544, 2)) #second parameter is for round upto
+
+# Assignment operator
+score = 0
+score += 1
+print(score)
