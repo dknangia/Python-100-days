0cacca7eb229e5104f0c65ee49637d6a1d97b384
Deepak Nangia <dknangia@gmail.com>
2024-11-27 20:49:53 -0500

    Rock paper scissor program

---
+++ Day 04 - Randomisation and Python Lists/rock-paper-scissor.py
@@ -0,0 +1,64 @@
+# Rock Paper Scissors ASCII Art
+# Rock beats scissor
+# Scissors beat paper
+# Paper beat rock
+# Rock
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+# Paper
+paper = '''
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+# Scissors
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+
+userInput = int(input("Enter your choice 0 for ROCK, 1 for PAPER and 2 for SCISSOR\n"))
+
+computerInput = -1
+if 3 > userInput >= 0:
+    computerInput = random.randint(0,2)
+    if computerInput == 0:
+        print(rock)
+    elif computerInput == 1:
+        print(paper)
+    elif computerInput == 2:
+        print(scissors)
+
+if userInput == computerInput:
+    print("Draw")
+elif userInput == 0 and computerInput == 2:
+    print ("You win")
+elif userInput == 2 and computerInput == 0:
+    print("You Win")
+elif userInput > computerInput:
+    print("You win")
+elif computerInput > userInput:
+    print("You loose")
+
+else:
+    print("Entered values is invalid")
+
+
