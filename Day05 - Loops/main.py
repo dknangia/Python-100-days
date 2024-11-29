001794a95387d6d212dd22808a9eff313119bee6
Deepak Nangia <dknangia@gmail.com>
2024-11-28 23:04:08 -0500

    Programming loops

--- Day05 - Loops/main.py
+++ Day05 - Loops/main.py
@@ -1,4 +1,22 @@
 fruits = ["Apple", "Pear", ["Mango", "Task2"]]
 
-for fruit in fruits:
-    print (fruit)
+# for fruit in fruits:
+#     print (fruit)
+#
+
+
+# Range function
+
+# for counter in range(0, 5):  #This will not include 5.
+#     print(counter)
+#
+# for counter in range (0, 10, 2):
+#     print(counter)
+
+
+sum_Of_Number =0
+
+for counter in range (1, 101):
+    sum_Of_Number += counter
+
+print(sum_Of_Number)
